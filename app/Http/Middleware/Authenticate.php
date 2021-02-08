4725f801914448f420b37607f87f19a45bdf6783
Gerardo <gmunozasca@gmail.com>
2021-02-07 21:22:48 -0500

    Add Auth by Query Params

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,6 +6,28 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+
+    protected function authenticate($request, array $guards)
+    {
+        if ($request->has('api_token')) {
+            $api_token = $request->input('api_token');
+            $request->headers->set('Authorization', "Bearer $api_token");
+        }
+
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        $this->unauthenticated($request, $guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
